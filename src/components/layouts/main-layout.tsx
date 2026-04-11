a7419f4b976426da7ef0d019d1a904253375e8b4
DaVid Justin <chaosdudeserad@gmail.com>
2026-04-12 00:41:37 +0800

    refactor: enhance MainLayout structure and ensure full-height layout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -2,12 +2,14 @@ import { NavBar } from "../navbar";
 
 export function MainLayout ({children} : { children: React.ReactNode }) {
     return (
-        <div className="relative w-full">
-            <div className="absolute inset-0 z-0">
-                {children}
-            </div>
-            <div className="relative z-10">
+        <div className="relative w-full min-h-screen">
+            {/* Navbar */}
+            <div className="relative z-20">
                 <NavBar />
+            </div>
+            {/* Page content */}
+            <div className="relative z-10">
+                {children}
             </div>
         </div>
     )
