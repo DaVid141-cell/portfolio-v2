579de5d60f835e3ce54c0011982f9fae1e7bda28
DaVid Justin <chaosdudeserad@gmail.com>
2026-04-15 03:04:36 +0800

    feat: add HeroImageHover component and integrate it into MainPage; update SocialIconFrame to use Font Awesome icons

--- src/components/ui/social-icon-frame.tsx
+++ src/components/ui/social-icon-frame.tsx
@@ -1,36 +1,42 @@
 import { Circle } from "lucide-react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const SocialLinks = [
+    { icon: faGithub, href: "https://github.com/DaVid141-cell" },
+    { icon: faLinkedin, href: "https://www.linkedin.com/in/david-justin-2092ba37a/" },
+    { icon: faFacebook, href: "https://www.facebook.com/seraddavidjustin" }
+]
 
 
 export function SocialIconFrame () {
     return (
-        <div className="flex flex-col items-center">
-            
+       <div className="flex flex-col items-center">
+
             {/* Top small outlined circle */}
-            <Circle size={40} fill="none" color="white" />
+            <Circle size={30} fill="none" color="white" />
 
-            {/* Connector line */}
-            <div className="w-px bg-white" style={{ height: "30px" }} />
+            {SocialLinks.map((social, index) => (
+                <div key={index} className="flex flex-col items-center">
+                    {/* Connector line */}
+                    <div className="w-px bg-white" style={{ height: "30px" }} />
 
-            {/* Large filled circle */}
-            <Circle size={70} fill="white" color="white" />
-
-            {/* Connector line */}
-            <div className="w-px bg-white" style={{ height: "30px" }} />
-
-            {/* Large filled circle */}
-            <Circle size={70} fill="white" color="white" />
-
-            {/* Connector line */}
-            <div className="w-px bg-white" style={{ height: "30px" }} />
-
-            {/* Large filled circle */}
-            <Circle size={70} fill="white" color="white" />
+                    {/* social icon circle */}
+                    <a
+                        href={social.href}
+                        target="_blank"
+                        className="w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center text-black text-2xl hover:opacity-80 transition-opacity"
+                    >
+                        <FontAwesomeIcon icon={social.icon} />
+                    </a>
+                </div>
+            ))}
 
             {/* Connector line */}
             <div className="w-px bg-white" style={{ height: "30px" }} />
 
             {/* Bottom small outlined circle */}
-            <Circle size={20} fill="none" color="white" />
+            <Circle className="cursor-grab" size={20} fill="none" color="white" />
 
         </div>
     )
--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,5 +1,6 @@
 import { MainLayout } from "../components/layouts/main-layout"
 import { GridBox } from "../components/ui/grid-boxes"
+import { HeroImageHover } from "../components/ui/hero-image-hover"
 import { SocialIconFrame } from "../components/ui/social-icon-frame"
 
 export default function MainPage() {
@@ -14,31 +15,34 @@ export default function MainPage() {
             <div className="relative">
 
                 {/* First Name box */}
-                <div>
-                    <h1>DAVID JUSTIN</h1>
+                <div className="absolute top-10 left-25">
+                    <h1 className="font-display text-8xl" >DAVID JUSTIN</h1>
                 </div>
 
                 {/*social media box */}
-                <div>
+                <div className="absolute left-390">
                     <SocialIconFrame/>
                 </div>
                 
                 {/* Center image here */}
-                <div>
-
+                <div className="absolute top-20 left-170 w-120">
+                    <HeroImageHover/>
                 </div>
 
                 {/*Small information */}
-                <div>
-                    <h3>
-                        Text
-                    </h3>
-                    <p>Samll Text</p>
+                <div className="absolute top-60 left-28 w-100 ">
+                    <article>
+                        <h2 className="text-3xl font-bold mb-2">
+                        I'm a Full Stack Developer
+                        </h2>
+                        <p className="text-lg text-justify">A software engineer passionate about turning ideas into real, functional products on the web. I craft digital experiences that are both functional and visually intentional  and always open to new things and new opportunities. Let's build something great together.</p>
+                    </article>
+                    
                 </div>
 
                 {/* Last Name box*/}
-                <div>
-                    <h1>Last Name</h1>
+                <div className="absolute top-140 left-330">
+                    <h1 className="font-display text-8xl">SERAD</h1>
                 </div>
 
             </div>
