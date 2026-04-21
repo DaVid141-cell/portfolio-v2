8c89c9f50c9b4260590cd6ea8baad4f3d5e37adf
DaVid Justin <chaosdudeserad@gmail.com>
2026-04-21 22:53:34 +0800

    fixed the main-layout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -4,11 +4,9 @@ export function MainLayout ({children} : { children: React.ReactNode }) {
     return (
         <div className="relative w-full min-h-screen">
             {/* Navbar */}
-            <div className="relative z-20">
                 <NavBar />
-            </div>
             {/* Page content */}
-            <div className="relative z-10">
+            <div className="">
                 {children}
             </div>
         </div>
