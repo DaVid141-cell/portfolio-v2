79be6d379eab876817ff34c126b8911c9ade29a8
DaVid Justin <chaosdudeserad@gmail.com>
2026-04-22 00:56:36 +0800

    fix the grid box hovering effect and fix the positioning of the contents

--- src/pages/hero-section.tsx
+++ src/pages/hero-section.tsx
@@ -5,23 +5,23 @@ import { SocialIconFrame } from "../components/ui/social-icon-frame";
 export function HeroSection () {
     return (
         <div className="relative w-full min-h-screen overflow-hidden">
-            <div className="absolute inset-0 -z-10">
+            <div className="absolute inset-0 z-10">
                 <GridBox />
             </div>
             {/* container of the contents */}
-            <div className="relative z-10 h-screen flex items-center translate-y-32">
+            <div className="absolute z-10 h-screen flex translate-y-32">
                 {/* First Name box */}
-                <div className="absolute top-10 left-25">
+                <div className="absolute top-10 left-25 w-125">
                     <h1 className="font-display text-8xl" >DAVID JUSTIN</h1>
                 </div>
 
                 {/*social media box */}
-                <div className="absolute left-390 top-0">
+                <div className="absolute left-420 top-0">
                     <SocialIconFrame/>
                 </div>
                 
                 {/* Center image here */}
-                <div className="absolute top-20 left-170 w-120">
+                <div className="absolute top-60 left-115 w-250">
                     <HeroImageHover/>
                 </div>
 
@@ -37,7 +37,7 @@ export function HeroSection () {
                 </div>
 
                 {/* Last Name box*/}
-                <div className="absolute top-140 left-330">
+                <div className="absolute top-160 left-380">
                     <h1 className="font-display text-8xl">SERAD</h1>
                 </div>
             </div>
