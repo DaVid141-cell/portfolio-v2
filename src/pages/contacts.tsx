786cb1e55dd4e98ea1d1ed8d28c37b83198c0285
DaVid Justin <chaosdudeserad@gmail.com>
2026-05-02 20:45:14 +0800

    implement contact form with email submission and loading state

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,13 +1,55 @@
-
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card"
+import { Card, CardContent } from "../components/ui/card"
 import { Label } from "../components/ui/label"
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 import { Textarea } from "../components/ui/textarea"
 import { PaperPlane } from "../components/ui/paper-plane"
+import { FEEDBACK_FORM_URL } from "../lib/api"
+import { useState } from "react"
 
 export default function ContactPage () {
+    const [ feedback, setFeedback ] = useState("");
+    const [ name, setIsName ] = useState("")
+    const [ email, setIsEmail ] = useState("")
+    const [ loading, setIsloading ] = useState(false);
+    const [ success, setIsSuccess ] = useState(false);
+    const [ error, setIsError ] = useState("");
+    const user = "itsdevsenpai09";
+    const domain = "gmail.com";
+    const emailAD = `${user}@${domain}`;
 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsError("");
+        setIsSuccess(false)
+
+        if (!feedback.trim() || !name.trim() || !email.trim()) {
+            setIsError("Please fill the form first before submitting")
+            return;
+        }
+
+        setIsloading(true)
+        try {
+            const response = await fetch(FEEDBACK_FORM_URL, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    message: feedback
+                }),
+            });
+            if (!response.ok) throw new Error("Failed to send the message.");
+            setIsSuccess(true)
+            setFeedback("");
+            setIsName("")
+            setIsEmail("")
+        } catch (error) {
+            setIsError("Failed to send the Message, Please try again.");
+        } finally {
+            setIsloading(false)
+        }
+    }
     
 
 
@@ -21,8 +63,8 @@ export default function ContactPage () {
                 
                 <div className="flex items-center">
                     
-                    <a href="" className="inline-flex items-center gap-2 px-6 py-3 cursor-pointer bg-[#7226FF]/20 hover:bg-[#A026FF]/30 text-[#FFD6F4] rounded-lg transition-all duration-300 border border-[#7226FF]">
-                        <PaperPlane/>itsdevsenpai09@gmail.com
+                    <a href={`mailto:${emailAD}`} className="inline-flex items-center gap-2 px-6 py-3 cursor-pointer bg-[#7226FF]/20 hover:bg-[#A026FF]/30 text-[#FFD6F4] rounded-lg transition-all duration-300 border border-[#7226FF]">
+                        <PaperPlane/>{emailAD}
                     </a>
                 </div>
             </div>
@@ -30,36 +72,66 @@ export default function ContactPage () {
             <div className="w-110">
                 <Card className="bg-foreground shadow-xl/60 w-full p-8">
                     <CardContent>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className="flex flex-col gap-6 text-white">
                                 <div className="grid gap-2">
                                 <Label className="font-bold" htmlFor="name">NAME</Label>
                                 <Input
-                                    
+                                    name="name"
+                                    value={name}
+                                    onChange={(e) => setIsName(e.target.value)}
                                     id="name"
-                                    type="name"
+                                    type="text"
                                     placeholder="John Kiddo"
+                                    autoComplete="off"
                                     required
                                 />
                                 </div>
                                 <div className="grid gap-2">
-                                    <Label className="font-bold" htmlFor="password">EMAIL</Label>
+                                    <Label className="font-bold" htmlFor="email">EMAIL</Label>
                                     <Input 
+                                        name="email"
+                                        value={email}
+                                        onChange={(e) => setIsEmail(e.target.value)}
                                         id="email" 
                                         type="email"
-                                        placeholder="example@gmail.com" 
+                                        placeholder="example@gmail.com"
+                                        autoComplete="off" 
                                         required 
                                     />
                                 </div>
 
                                 <div>
-                                    <Label className="font-bold" htmlFor="message">MESSAGE</Label>
-                                    <Textarea className="mt-2" placeholder="Type your message here..." required />
+                                    <Label className="font-bold" htmlFor="feedback">MESSAGE</Label>
+                                    <Textarea 
+                                        id="feedback"
+                                        name="feedback"
+                                        value={feedback}
+                                        onChange={(e) => setFeedback(e.target.value)}
+                                        className="mt-2" 
+                                        placeholder="Type your message here..."
+                                        autoComplete="off" 
+                                        required />
                                 </div>
                             </div>
+                            {error && (
+                                <p>{error}</p>
+                            )}
+                            {success && (
+                                <p>{"Successfully Sent."}</p>
+                            )}
 
-                            <Button type="submit" className="w-full mt-4 p-6 cursor-pointer">
-                                <PaperPlane/>Send Message
+                            <Button 
+                                type="submit" 
+                                className="w-full mt-4 p-6 cursor-pointer"
+                                disabled={loading}
+                            >
+                                {loading ? "loading..." : (
+                                    <>
+                                        <PaperPlane />
+                                        Send Message
+                                    </>
+                                )}
                             </Button>
                         </form>
                     </CardContent>
